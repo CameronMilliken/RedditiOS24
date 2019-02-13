7ffb155d7f0163debe5ef0ea06a4fa60cc7014b3
Cameron <cameron.milliken@gmail.com>
2019-02-13 11:41:51 -0700

    Initial commit.

---
+++ RedditiOS24/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RedditiOS24
+//
+//  Created by Cameron Milliken on 2/13/19.
+//  Copyright © 2019 Cameron Milliken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RedditiOS24/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RedditiOS24
+//
+//  Created by Cameron Milliken on 2/13/19.
+//  Copyright © 2019 Cameron Milliken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
